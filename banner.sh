b755d588d0f6dd697f64bf4c0a1cde09006cee99
Jorge A. Ruiz <jorgearuiz@me.com>
2019-02-14 20:20:48 -0800

    added banners

---
+++ banner.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+                                                                                                                                                                                                        
+# Variables                                                                                                                                                                                             
+ipaddress=$(hostname -i)  
+
+
+--------------------------------------------------------------------------
+
+███╗   ███╗██╗███████╗ █████╗ 	WELCOME TO
+████╗ ████║██║██╔════╝██╔══██╗	MANAGEMENT INFORMATION SYSTEMS ASSOCIATION
+██╔████╔██║██║███████╗███████║	
+██║╚██╔╝██║██║╚════██║██╔══██║	18111 Nordhoff St Northridge, 
+██║ ╚═╝ ██║██║███████║██║  ██║	CA 91330 United States
+╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
+								 
+THE BRIDGE BETWEEN BUSINESS		AWS INFRASTRUCTURE | ${ipaddress}					
+AND TECHNOLOGY					
+								------------------------------------------
+PLEASE REFER TO THE				|	FOR MORE INFORMATION CONTACT:
+DOCUMENTATION IF YOU			|	Email: jorge.ruiz.750@my.csun.edu
+ARE UNSURE OF WHAT YOU ARE		|	Twitter: twitter.com/ruizjorgealt
+DOING HERE !					|	LinkedIn: linkedin.com/in/ruizjorgea 
+								|   Website: jorgearuiz.net
+--------------------------------------------------------------------------
---
+++ login.sh
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------
+
+You are attempting to logon to Management Information Systems Association
+Amazon Web Services infrastructure.
+
+This infrastructure is provided only for authorized MISA use.
+
+All activities on this system is subject to monitoring for authorized use.
+  
+Unauthorized use may subject you to criminal prosecution, under the statues of
+the Digital Millennium Act. Use of this infrastructure constitutes
+acknowledgment and acceptance of all CSUN Information Technology related
+policies.  University Policies are published at the following URL:
+http://www.csun.edu/afvp/university-policies-procedures/
+
+--------------------------------------------------------------------------
