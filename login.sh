1e2035812299e770bd49a095387cd191092d63ba
Jorge A. Ruiz <jorgearuiz@icloud.com>
2019-02-14 20:49:39 -0800

    update login msg

--- login.sh
+++ login.sh
@@ -1,16 +1,13 @@
---------------------------------------------------------------------------
-
-You are attempting to logon to Management Information Systems Association
-Amazon Web Services infrastructure.
-
-This infrastructure is provided only for authorized MISA use.
-
-All activities on this system is subject to monitoring for authorized use.
-  
-Unauthorized use may subject you to criminal prosecution, under the statues of
-the Digital Millennium Act. Use of this infrastructure constitutes
-acknowledgment and acceptance of all CSUN Information Technology related
-policies.  University Policies are published at the following URL:
-http://www.csun.edu/afvp/university-policies-procedures/
-
---------------------------------------------------------------------------
+echo "--------------------------------------------------------------------------"
+echo " "
+echo "You are attempting to logon to Management Information Systems Association"
+echo "Amazon Web Services infrastructure."
+echo " "
+echo "This infrastructure is provided only for authorized MISA use."
+echo " "
+echo "All activities on this system is subject to monitoring for authorized use."
+echo " "
+echo "Unauthorized use may subject you to criminal prosecution, under the statues of"
+echo "the Digital Millennium Act."
+echo " "
+echo "--------------------------------------------------------------------------"
