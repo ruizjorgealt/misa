a03d51fe0822db6379a8ecee2b8aecbf4a701256
Jorge A. Ruiz <jorgearuiz@me.com>
2019-02-14 20:33:34 -0800

    fix banner formatting

--- banner.sh
+++ banner.sh
@@ -4,21 +4,21 @@
 ipaddress=$(hostname -i)  
 
 
---------------------------------------------------------------------------
-
-███╗   ███╗██╗███████╗ █████╗ 	WELCOME TO
-████╗ ████║██║██╔════╝██╔══██╗	MANAGEMENT INFORMATION SYSTEMS ASSOCIATION
-██╔████╔██║██║███████╗███████║	
-██║╚██╔╝██║██║╚════██║██╔══██║	18111 Nordhoff St Northridge, 
-██║ ╚═╝ ██║██║███████║██║  ██║	CA 91330 United States
-╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
-								 
-THE BRIDGE BETWEEN BUSINESS		AWS INFRASTRUCTURE | ${ipaddress}					
-AND TECHNOLOGY					
-								------------------------------------------
-PLEASE REFER TO THE				|	FOR MORE INFORMATION CONTACT:
-DOCUMENTATION IF YOU			|	Email: jorge.ruiz.750@my.csun.edu
-ARE UNSURE OF WHAT YOU ARE		|	Twitter: twitter.com/ruizjorgealt
-DOING HERE !					|	LinkedIn: linkedin.com/in/ruizjorgea 
-								|   Website: jorgearuiz.net
---------------------------------------------------------------------------
+echo "----------------------------------------------------------------------------"
+echo " "
+echo "███╗   ███╗██╗███████╗ █████╗     WELCOME TO"
+echo "████╗ ████║██║██╔════╝██╔══██╗    MANAGEMENT INFORMATION SYSTEMS ASSOCIATION"
+echo "██╔████╔██║██║███████╗███████║"
+echo "██║╚██╔╝██║██║╚════██║██╔══██║    18111 Nordhoff St Northridge,"
+echo "██║ ╚═╝ ██║██║███████║██║  ██║    CA 91330 United States"
+echo "╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝"
+echo " "								 
+echo "THE BRIDGE BETWEEN BUSINESS       AWS INFRASTRUCTURE | ${ipaddress}"
+echo "AND TECHNOLOGY"
+echo "                              ----------------------------------------------"
+echo "PLEASE REFER TO THE           | IN CASE OF EMERGENCY CONTACT:               "
+echo "DOCUMENTATION IF YOU          | Email: jorge.ruiz.750@my.csun.edu           "
+echo "ARE UNSURE OF WHAT YOU ARE    | Twitter: twitter.com/ruizjorgealt           "
+echo "DOING HERE !                  | LinkedIn: linkedin.com/in/ruizjorgea        "
+echo "                              | Website: jorgearuiz.net                     "
+echo "----------------------------------------------------------------------------"
